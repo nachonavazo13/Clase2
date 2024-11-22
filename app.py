82ebb7b99eadbb81c27618413856019282f9f8e7
nachonavazo13 <nachonavazo13@hotmail.com>
2024-11-22 14:02:43 -0300

    Probando alguno componentes.

--- app.py
+++ app.py
@@ -8,18 +8,67 @@ from PIL import Image
 df = pd.read_csv("data/match_result.csv")
 
 def main():
-    st.title("Trabajando con datos en Streamlit")
-    st.dataframe(df)
-    st.header("Usando table.")
-    st.table(df)
-    st.header("Usando json.")
-    st.json({"clave":"valor"})
+    st.title("Trabajando con componentes en Streamlit")
 
-    codigo = """
-    def saludando(nombre:
-        print("Hola" + nombre))"""
-    st.code(codigo, language= "python")
+    posicion = st.selectbox("Seleccione una posicion",["Portero", "Defensa", "Mediocampista", "Delantero"])
+    st.write(f"La posicion seleccionada es: {posicion}")
 
+    opciones = st.multiselect("Seleccione una posicion",["Portero", "Defensa", "Mediocampista", "Delantero"])
+    st.write(f"La posicion seleccionada es: {opciones}")
+
+    edad = st.slider('Selecione su edad: ',
+                    min_value=1, max_value=100, value=20, step=1)
+    st.write(f"Su edad es : {edad}")
+
+    nivel = st.select_slider(
+        "Selecciones su nivel: ",
+        ["Muy bajo", "Bajo", "Medio", "Alto", "Muy alto"])
+    st.write(f"Su nivel es: {nivel}")
+
+    opc = st.radio('Seleccione una opcion: ',["Portero", "Defensa", "Mediocampista", "Delantero"])
+    st.write(f'La opcion es: {opc}')
+
+    check = st.checkbox("Acepto las condiciones.")
+    if check:
+        st.write("Has aceptado las condiciones")
+    else:
+        st.write("No has aceptado las condiciones")
+
+    rta = st.button('Hola Nacho')
+    if rta:
+        st.write('Que haces Nacho')
+        st.write(rta)
+    else:
+        st.write('-')
+        st.write(rta)
+
+    tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+    with tab1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    with tab2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    with tab3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+        
+
+
+    # TRABAJANDO CON TABLAS.
+    # st.dataframe(df)
+    # st.header("Usando table.")
+    # st.table(df)
+    # st.header("Usando json.")
+    # st.json({"clave":"valor"})
+
+    # codigo = """
+    # def saludando(nombre:
+    #     print("Hola" + nombre))"""
+    # st.code(codigo, language= "python")
+
+    # TRABAJANDO CON TEXTO.
     # st.header("Este es el encabezado.")
     # st.subheader("Este es el sub-encabezado.")
     # st.text("Esto es un texno normal.")
